b477aea9264edab3156e752a585ee6aa137d47eb
tdubourg <plop@plop.fake>
2013-04-20 18:50:13 +0200

    The main.lua file now contains a pretty good example of hitbox display together with sprite display

--- main.lua
+++ main.lua
@@ -4,20 +4,32 @@ require('game_object')
 local objects, physics_obj = {}, {}
 local timer = 0
 local INTERVAL = 0.2
+
+SPRITES_DIR = "resources/sprites/"
+
 function love.load()
 	PhysicsComponent.init()
 	objects[0] = GameObject.new()
-	physics_obj[0] = PhysicsComponent.new(PhysicsComponent.SHAPE_TYPES.R, 100, 0, false, {width=100, height=50}) -- for now, all game objects are dynamic
-	addPhysicsAndDisplayToGameObject(objects[0], physics_obj[0])
+	local SPRITE_W = 223
+	local SPRITE_H = 226
+	-- We are creating hitboxes that are 90% size of the sprite, so that we can see hitboxes interactions
+	-- And the sprite at the same time
+	local RECT_HITBOX_WIDTH = SPRITE_W * 0.9
+	local RECT_HITBOX_HEIGHT = SPRITE_H * 0.9
+	physics_obj[0] = PhysicsComponent.new(PhysicsComponent.SHAPE_TYPES.R, 100, 0, false, {width=RECT_HITBOX_WIDTH, height=RECT_HITBOX_HEIGHT}) -- for now, all game objects are dynamic
+	local disp1 = DisplayComponent.new(SPRITES_DIR .. "star.jpeg")
+	addPhysicsAndDisplayToGameObject(objects[0], physics_obj[0], disp1)
 	
 	objects[1] = GameObject.new()
-	physics_obj[1] = PhysicsComponent.new(PhysicsComponent.SHAPE_TYPES.C, 0, 0, false, {r=50}) -- for now, all game objects are dynamic
-	addPhysicsAndDisplayToGameObject(objects[1], physics_obj[1])
+	local CIRCLE_HITBOX_RADIUS = SPRITE_H * 0.9 / 2.0
+	physics_obj[1] = PhysicsComponent.new(PhysicsComponent.SHAPE_TYPES.C, 0, 0, false, {r=CIRCLE_HITBOX_RADIUS}) -- for now, all game objects are dynamic
+	local disp2 = DisplayComponent.new(SPRITES_DIR .. "star.jpeg")
+	addPhysicsAndDisplayToGameObject(objects[1], physics_obj[1], disp2)
 end
 
 function addPhysicsAndDisplayToGameObject( obj, phy_obj, disp_obj )
 	obj:add_component('physics', phy_obj)
-	-- obj:add_component('display', disp_obj)
+	obj:add_component('display', disp_obj)
 end
 
 function love.update(dt)
