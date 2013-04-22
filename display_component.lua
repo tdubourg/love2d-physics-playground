6f5eef3075f141ba5df5d9158369840eca9d16dd
tdubourg <plop@plop.fake>
2013-04-22 11:10:16 +0200

    spaces

--- display_component.lua
+++ display_component.lua
@@ -12,30 +12,30 @@ function DisplayComponent.new(sprite_path, draw_centered, offset)
     self.offset_x = 0
     self.offset_y = 0
     if draw_centered == nil then
-    	self.draw_centered = true
+        self.draw_centered = true
     else
-    	self.draw_centered = draw_centered
-    	if nil != offset then
-    		self.offset_x = offset.x
-    		self.offset_y = offset.y
-    	end
+        self.draw_centered = draw_centered
+        if nil != offset then
+            self.offset_x = offset.x
+            self.offset_y = offset.y
+        end
     end
     return self
 end
 
 function DisplayComponent:attach_to( game_object )
-	self.go = game_object
+    self.go = game_object
 end
 
 function DisplayComponent:update( dt, game_object, args )
-	-- @TODO : Store useful things
-	self.last_dt = dt
+    -- @TODO : Store useful things
+    self.last_dt = dt
 end
 
 function DisplayComponent:draw()
-	if self.draw_centered then
-		love.graphics.draw(self.sprite, self.go.centerx - self.w / 2.0, self.go.centery - self.h / 2.0)
-	else
-		love.graphics.draw(self.sprite, self.go.posx + self.offset_x, self.go.posy + self.offset_y)
-	end
+    if self.draw_centered then
+        love.graphics.draw(self.sprite, self.go.centerx - self.w / 2.0, self.go.centery - self.h / 2.0)
+    else
+        love.graphics.draw(self.sprite, self.go.posx + self.offset_x, self.go.posy + self.offset_y)
+    end
 end
